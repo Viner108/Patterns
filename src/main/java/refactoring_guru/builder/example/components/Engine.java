74c8543119030cff8664e20370d30ee67af9781c
Viner108 <andrukovichl@mail.ru>
2023-05-08 13:34:02 +0300

    builde commit

---
+++ src/main/java/refactoring_guru/builder/example/components/Engine.java
@@ -0,0 +1,30 @@
+package refactoring_guru.builder.example.components;
+
+public class Engine {
+    private final double volume;
+    private double mileage;
+    private boolean started;
+    public Engine(double volume,double mileage){
+        this.volume=volume;
+        this.mileage=mileage;
+    }
+    public void on(){
+        started=true;
+    }
+    public void off(){
+        started=false;
+    }
+    public void go(double mileage){
+        if (started){
+            this.mileage+=mileage;
+        }else {
+            System.out.println("Cannot go(), you must start engine first!");
+        }
+    }
+    public double getVolume(){
+        return volume;
+    }
+    public double getMileage(){
+        return mileage;
+    }
+}
