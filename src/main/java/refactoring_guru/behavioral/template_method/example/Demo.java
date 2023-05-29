aefb0e291be49108ccbbf1df794615b245506b80
Viner108 <andrukovichl@mail.ru>
2023-05-29 12:16:44 +0300

    template_method commit

---
+++ src/main/java/refactoring_guru/behavioral/template_method/example/Demo.java
@@ -0,0 +1,30 @@
+package refactoring_guru.behavioral.template_method.example;
+
+import refactoring_guru.behavioral.template_method.example.networks.Facebook;
+import refactoring_guru.behavioral.template_method.example.networks.Network;
+import refactoring_guru.behavioral.template_method.example.networks.Twitter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Network network = null;
+        System.out.println("Input user name: ");
+        String userName = reader.readLine();
+        System.out.println("Input password: ");
+        String password = reader.readLine();
+        System.out.println("Input message: ");
+        String message = reader.readLine();
+        System.out.println("\nChoose social network for posting message.\n" + "1 - Facebook\n" + "2 - Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+        if (choice == 1) {
+            network = new Facebook(userName, password);
+        } else if (choice == 2) {
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
+    }
+}
