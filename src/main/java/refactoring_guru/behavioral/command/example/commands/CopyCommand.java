9142a6de71e18211b19cc4ca632036b9d0fce99e
Viner108 <andrukovichl@mail.ru>
2023-05-30 12:12:00 +0300

    command commit

---
+++ src/main/java/refactoring_guru/behavioral/command/example/commands/CopyCommand.java
@@ -0,0 +1,14 @@
+package refactoring_guru.behavioral.command.example.commands;
+
+
+import refactoring_guru.behavioral.command.example.editor.Editor;
+
+public class CopyCommand extends Command{
+    public CopyCommand(Editor editor){
+        super(editor);
+    }
+    public boolean execute(){
+        editor.clipboard=editor.textField.getSelectedText();
+        return false;
+    }
+}
